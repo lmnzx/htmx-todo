3eeb4d1e62476a29cdf740a00d25d4e2a9a39093
Sayan Mallick <snyxmk@gmail.com>
2023-10-26 11:50:43 +0530

    minimal html

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse 
-from starlette.responses import FileResponse
+from fastapi import FastAPI, Request, Form, status
+from fastapi.responses import HTMLResponse, RedirectResponse 
 from fastapi.templating import Jinja2Templates
+from typing import Annotated
 import sqlite3
 import tempfile
 from pathlib import Path 
@@ -24,9 +24,34 @@ conn.commit()
 @app.get('/', response_class=HTMLResponse)
 async def get(request: Request):
     todos = c.execute('SELECT * FROM todos ORDER BY id').fetchall()
-    c.execute("SELECT COUNT(*) FROM todos")
+    c.execute('SELECT COUNT(*) FROM todos')
     total = c.fetchone()[0]
-    c.execute("SELECT COUNT(*) FROM todos WHERE done")
+    c.execute('SELECT COUNT(*) FROM todos WHERE done')
     done = c.fetchone()[0]
     count = {'done': done, 'total': total}
     return template.TemplateResponse('index.html', {'request': request, 'todos': todos, 'count': count})
+
+@app.post('/add')
+async def add(title: Annotated[str, Form()]):
+    if title:
+        c.execute('INSERT INTO todos (title, done) VALUES (?, 0)', (title, ))
+        conn.commit()
+        print(f'new todo is added id-> {c.lastrowid}')
+
+    return RedirectResponse(url='/', status_code=status.HTTP_303_SEE_OTHER)
+
+@app.get('/delete/{id}')
+async def delete(id):
+    c.execute('DELETE FROM todos WHERE id=?', (id,))
+    conn.commit()
+    return RedirectResponse(url='/', status_code=status.HTTP_303_SEE_OTHER)
+
+@app.get('/update/{id}')
+async def update(id):
+    todo = c.execute('SELECT done FROM todos WHERE id=?', (id,)).fetchone()[0]
+    if todo is 0:
+        c.execute('UPDATE todos SET done=true WHERE id=?', (id,))
+    else:
+        c.execute('UPDATE todos SET done=false WHERE id=?', (id,))
+    conn.commit()
+    return RedirectResponse(url='/', status_code=status.HTTP_303_SEE_OTHER)
